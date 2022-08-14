37458efc3901034aa451d2a22ab0343a693dd17a
Nandini <87466914+Nandinikonduru22@users.noreply.github.com>
2022-08-14 14:11:30 +0530

    even or  odd program

---
+++ even or odd.py
@@ -0,0 +1,12 @@
+num = int(input("Enter any number: "))
+flag = num%2
+if flag == 0:
+    print(num, "is an even number")
+elif flag == 1:
+    print(num, "is an odd number")
+else:
+    print("Error, Invalid input")
+
+
+o/p:-Enter any number: 55
+55 is an odd number
